c128d1f04a7fe7ff412d0bc4d6bf7695ca62dce5
Andrej Kirejeŭ <gs1994@gmail.com>
2023-12-29 12:53:14 +0100

    Update age.ts

--- src/age.ts
+++ src/age.ts
@@ -1,3 +1,7 @@
 export const getAgeString = (years: number) => {
-  return `The age of the person: ${years} years.`;
+  if (age > 0 && age < 180) {
+    return `The age of the person: ${years} years.`;
+  } else {
+    return 'Invalid age!';
+  }
 }
